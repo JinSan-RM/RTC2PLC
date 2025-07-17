8496f5713a881727d2c2a3fb7af25b6e8a0a2fb7
Seongmin <36837884+yhjogun@users.noreply.github.com>
2025-07-17 13:17:30 +0900

    [RTC2RPi] jsm UI관련 수정

--- RPi/ui/screens/manualscreen.py
+++ RPi/ui/screens/manualscreen.py
@@ -18,32 +18,43 @@ class ManualScreen(Screen):
     # 운전 모드 조작
     def change_mode(self):
         print("change mode pressed.")
-        return
-    
+
     # 운전 시작
     def start_process(self):
         print("start_process pressed")
-        return
-    
+
     # 운전 정지
     def stop_process(self):
         print("stop_process pressed")
-        return
-    
+
     # 원점
     def go_to_zeropoint(self):
         print("go_to_zeropoint pressed")
-        return
-    
+
     # 고장없음
     def check_breakdown(self):
         print("check_breakdown pressed")
-        return
-    
+
     # 각 파트 운전
     def on_start_manual(self, part_name, part_num):
         self.gpio.write_pin(PIN_MAPPING[part_name][part_num], 1)
-    
+
     # 각 파트 정지
     def on_stop_manual(self, part_name, part_num):
         self.gpio.write_pin(PIN_MAPPING[part_name][part_num], 0)
+
+    # 위치 1
+    def on_first_size(self):
+        print("on_first_size pressed")
+
+    # 위치 2
+    def on_second_size(self):
+        print("on_second_size pressed")
+
+    # 위치 3
+    def on_third_size(self):
+        print("on_third_size pressed")
+
+    # 위치 4
+    def on_fourth_size(self):
+        print("on_fourth_size pressed")
